a5dbd3be044a2a4957a29ed0dac83377a5962929
stanislaviskandarov <101622311+stanislaviskandarov@users.noreply.github.com>
2022-03-15 13:15:19 +0600

    Initial commit

---
+++ main/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="special-layout">
+    <h1>Hello</h1>
+    <Content />
+  </div>
+</template>
+
+<script>
+export default {
+  fileName: "SomePageLayout.vue",
+};
+</script>
+
+
+<style src="prismjs/themes/prism-tomorrow.css"></style>
+
+<style scoped>
+</style>
